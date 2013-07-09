be07fb3ea9d5764501ee5ee93a909584c31ab551
Sage Imel <sage@cat.pdx.edu>
2013-07-09 10:51:51 -0700

    fix typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class ssh_keys {
   include ssh_keys::params
   $_homedir_base = $ssh_keys::params::homedir_locations["${osfamlily}"]
-  $homedir_base = $possible_homedir_base ? {
+  $homedir_base = $_homedir_base ? {
     undef   => $ssh_keys::params::fallback_homedir_location,
     default => $_homedir_base
   }
