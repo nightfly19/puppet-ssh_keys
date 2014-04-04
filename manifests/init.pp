93a793b4ee6dcae35af8c507fc1614d4cd675b3b
sulaweyo <sledge.sulaweyo@gmail.com>
2014-04-04 10:51:37 +0200

    follow style guide

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class ssh_keys {
   include ssh_keys::params
-  $_homedir_base = $ssh_keys::params::homedir_locations["${osfamlily}"]
+  $_homedir_base = $ssh_keys::params::homedir_locations[ $::osfamlily ]
   $homedir_base = $_homedir_base ? {
     undef   => $ssh_keys::params::fallback_homedir_location,
     default => $_homedir_base
